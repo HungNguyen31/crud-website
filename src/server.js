0e7b7bb744990c6c1256147161da379b64efd9b0
Hung Nguyen <nxhung31@gmail.com>
2023-03-13 16:12:26 +0700

    update project

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+
+configViewEngine(app);
+
+// app.get("/", (req, res) => {
+//   res.sendFile("./index.html", { root: __dirname });
+// });
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
